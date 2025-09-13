e735da759a1f64414cc63750d3579d33b2bb2543
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-09-13 07:02:33 +0200

    feat: render semantic-release config file

--- src/ci_starter/cli/__init__.py
+++ src/ci_starter/cli/__init__.py
@@ -3,11 +3,13 @@ from logging.config import dictConfig as configure_logging
 from pathlib import Path
 
 from click import Path as ClickPath
-from click import argument, command, echo, option
+from click import command, echo, option
 
 from .. import __version__ as version
+from .. import generate_semantic_release_config
+from ..errors import RemoteNotFoundError
 from ..logging_conf import logging_configuration
-from .callbacks import set_workdir
+from .callbacks import set_module_name, set_workdir
 from .validations import validate_test_group, validate_workflow_file_name
 
 configure_logging(logging_configuration)
@@ -18,14 +20,13 @@ logger = getLogger(__name__)
 @command()
 @option(
     "-C",
-    "--change-dir",
+    "--project-path",
     "workdir",
     default=".",
     type=ClickPath(exists=True, dir_okay=True, writable=True, allow_dash=False, path_type=Path),
     callback=set_workdir,
 )
-@argument("package_name")
-@argument("module_name")
+@option("-m", "--module_name", callback=set_module_name)
 @option(
     "--workflow-file-name",
     default="continuous_delivery.yml",
@@ -37,15 +38,17 @@ logger = getLogger(__name__)
 def cli(
     workdir: Path,
     module_name: str,
-    package_name: str,
     workflow_file_name: Path,
     test_group: str,
     test_command: str,
 ) -> None:
-    echo(f"Hello from ci-starter version {version}!")
-    logger.debug("package_name = %s", package_name)
+    echo(f"ci-starter {version}!")
     logger.debug("module_name = %s", module_name)
     logger.debug("workflow_file_name = %s", workflow_file_name)
     logger.debug("workdir = %s", workdir)
     logger.debug("test_group = %s", test_group)
     logger.debug("test_command = %s", test_command)
+    try:
+        generate_semantic_release_config(workdir.project, workdir.config)
+    except RemoteNotFoundError as err:
+        logger.exception(err)
