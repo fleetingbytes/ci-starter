e13912659882d1fc97f1aa55e45bde53711fbcb5
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-10-10 02:08:56 +0200

    docs: update documentation in update-test_update_actions.sh

--- scripts/update-test_update_actions.sh
+++ scripts/update-test_update_actions.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 # Rationale:
-# Ci-starter has the subcommand update-actions to update an end-user's project's workflow files to that they
-# use the latest versions of the GitHub actions. This updates the actions in all of the end-user's project's
-# workflow files, regardless whether these workflow files have been created with ci-starter or manually.
-# 
+# Ci-starter's subcommand update-actions updates an end-user's project's workflow files so that they
+# use the latest versions of the GitHub actions. This updates the action versions in all of the end-user's
+# project's workflow files, regardless whether these workflow files have been created with ci-starter or
+# manually.
+#
 # Ci-starter project has an end-to-end test where we test if this update-actions subcommand works properly.
 # In that test we check if the handful of actions ci-starter uses in its workflow templates are updated
-# correctly and match their respective expeced commit SHA and version. The expected current commit SHA
+# correctly and match their respective expected commit SHA and version. The expected current commit SHA
 # and version of each GitHub action needs to be hard-coded in the end-to-end test.
 # However, as time goes on, the expected commit SHAs and versions of the updated actions from ci-starter's
 # templates change because these actions, too, are getting new releases from time to time.
 #
 # The maintainer of the ci-starter project needs a way to update the end-to-end test's code
-# so that the expected versions match the actual current version of the actions. Doing this manually is
-# cumbersome. This script was written to update the code of the end-to-end test of the
-# update-actions subcommand programatically.
+# so that the expected versions match the actual current versions of the actions. Doing this manually is
+# cumbersome. This script was written to update the code of the end-to-end test of the update-actions
+# subcommand programatically.
 #
 # Usage:
 # - make sure you are in the ci-starter's project directory
 # - uv executable is in $PATH
 # - ci-starter project is installed with the lint dev-dependency group
-# - for frequent use provide a GitHub personal access token to avoid getting rate-limited
+# - provide a GitHub personal access token to avoid getting rate-limited:
 # CI_STARTER_GH_API_TOKEN=<your-access-token-for-github> ./scripts/update-test_update_actions.sh
 
 WORKFLOWS=./.github/workflows
 TEST_FILE=./tests/e2e/test_update_actions.py
 
-actions_from_all_workflows() {
+find_lines_where_action_is_pinned_to_sha_and_commented_with_version() {
     find "$WORKFLOWS" -type f -name "*.yml" \
         | xargs sed -nE 's$uses: ([^[:space:]]+)/([^[:space:]]+)@([0-9a-f]{40})  # v([^[:space:]]+)$\1 \2 \3 \4$p' \
         | sort | uniq
 }
 
 get_actions_from_all_workflows_with_their_latest_commits_and_versions() {
-    actions_from_all_workflows | while read owner repo old_commit old_version; do
+    find_lines_where_action_is_pinned_to_sha_and_commented_with_version | while read owner repo old_commit old_version; do
         url="https://api.github.com/repos/$owner/$repo/tags"
 
         if [ -n "$CI_STARTER_GH_API_TOKEN" ]; then
