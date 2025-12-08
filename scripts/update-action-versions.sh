34d0b0cd3dceca3689bb6b182a5e06fd1689ff25
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-12-08 19:48:27 +0100

    ci: update action versions

---
+++ scripts/update-action-versions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Usage: run this from project directory: ./scripts/update-action-versions
+
+uv run -- ci-start update-actions
+./scripts/update-test_update_actions.sh
+
+git add .
+git commit -m "ci: update action versions"
+git push
