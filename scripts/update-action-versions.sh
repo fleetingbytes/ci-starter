aa5447119ff961140e5c59257750599af19e05ee
fleetingbytes <7075397+fleetingbytes@users.noreply.github.com>
2025-12-14 19:59:02 +0100

    ci: update action versions

--- scripts/update-action-versions.sh
+++ scripts/update-action-versions.sh
@@ -1,14 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-# Usage: run this from project directory: ./scripts/update-action-versions
+get_absolute_path_to_parent_dir_of_this_script() {
+    local absolute_path="$(readlink -f -- "$1")"
+    local parent_dir="$(dirname "$absolute_path")"
+    printf "$parent_dir"
+}
 
-if git pull; then
+SCRIPT_DIR="$(get_absolute_path_to_parent_dir_of_this_script "$0")"
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
 
-    uv run -- ci-start update-actions
-    ./scripts/update-test_update_actions.sh
+if git -C "$PROJECT_DIR" pull; then
 
-    git add .
-    git commit -m "ci: update action versions"
-    git push
+    uv --directory "$PROJECT_DIR" run -- ci-start --project-path "$PROJECT_DIR" update-actions
+    "$SCRIPT_DIR"/update-test_update_actions.sh
+
+    git -C "$PROJECT_DIR" add .
+    git -C "$PROJECT_DIR" commit -m "ci: update action versions"
+    git -C "$PROJECT_DIR" push
 
 fi
